9d0e02d81d92bf224668df734da88ad14ed1f231
bcmwuziqi <bcmwuziqicpl@outlook.com>
2025-01-14 17:56:02 +0800

    Update kms.cpp

--- kms.cpp
+++ kms.cpp
@@ -1,1 +1,11 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+string s;
+int main(){
+    cout<<"Your key...\nOk,your name:";
+    cin>>s;
+    cout<<"\nOk.\n...look...go...\n";
+    system("jihuo-kms.bat");
+    cout<<"Ok,"<<s;
+    return 0;
+}
